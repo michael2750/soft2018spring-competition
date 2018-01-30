97b41257efb672f73df0e2c6b447ee79fd274434
Michael Daugbjerg <2750daugbjerg@gmail.com>
2018-01-30 15:25:06 +0100

    test

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+﻿#!/bin/bash
 # This is a mysterious and weird bash script with an error somewhere..
-function x { ehco $1 | tr 'a-zA-Z' 'n-za-mN-ZA-M'; }; eval $(x 'juvyr :;qb sbe v va {1..20}
+function x { echo $1 | tr 'a-zA-Z' 'n-za-mN-ZA-M'; }; eval $(x 'juvyr :;qb sbe v va {1..20}
 {19..2};qb cevags "\r[31;1z%${v}f \e" █;fyrrc 0.02;qbar;qbar'); exit
 A=(105557416050692  77176372792420  111263447882950 111089501671460 111298948435175)
 A+=(140745122766848 140746759356416 140738707374080 140739778977792 140754249564160)
